5403be912bbd2270a037dcfef2b51c748d5a7b7e
Nikkel Heesen <heesen.nikkel@madnessstudio.de>
2018-12-11 17:53:14 +0100

    Moved library definitions and preprocessor defines from cpp to cmake

--- CppVersion/source/EulerAdventure.cpp
+++ CppVersion/source/EulerAdventure.cpp
@@ -1,31 +1,4 @@
-
-#define SFML_STATIC
 #include <SFML/Window.hpp>
-
-#ifndef _DEBUG
-	#pragma comment(lib, "sfml-graphics-s.lib")
-	#pragma comment(lib, "sfml-window-s.lib")
-	#pragma comment(lib, "sfml-audio-s.lib")
-	#pragma comment(lib, "sfml-network-s.lib")
-	#pragma comment(lib, "sfml-system-s.lib")
-#else
-	#pragma comment(lib, "sfml-graphics-s-d.lib")
-	#pragma comment(lib, "sfml-window-s-d.lib")
-	#pragma comment(lib, "sfml-audio-s-d.lib")
-	#pragma comment(lib, "sfml-network-s-d.lib")
-	#pragma comment(lib, "sfml-system-s-d.lib")
-#endif
-#pragma comment(lib, "opengl32.lib")
-#pragma comment(lib, "freetype.lib")
-#pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "gdi32.lib")
-#pragma comment(lib, "openal32.lib")
-#pragma comment(lib, "flac.lib")
-#pragma comment(lib, "vorbisenc.lib")
-#pragma comment(lib, "vorbisfile.lib")
-#pragma comment(lib, "vorbis.lib")
-#pragma comment(lib, "ogg.lib")
-#pragma comment(lib, "ws2_32.lib")
 
 
 int main(char** argv, int argc)
